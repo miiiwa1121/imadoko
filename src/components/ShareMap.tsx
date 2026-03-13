ac503882512a2381255f24f90fae44526b1b185d
三輪飛翔 <t.miwayagish.jp@PC009A-TMWA.local>
2026-03-14 06:27:23 +0900

    ok

--- src/components/ShareMap.tsx
+++ src/components/ShareMap.tsx
@@ -58,9 +58,9 @@ export default function ShareMap({
     >
       <MapUpdater focusLocation={focusLocation} focusKey={focusKey} />
       <TileLayer
-        attribution="&copy; <a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>"
-        url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
-      />
+        attribution='Tiles &copy; Esri'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+      />      
       
       {participants.map((p) => {
         if (p.lat === null || p.lng === null) return null;
