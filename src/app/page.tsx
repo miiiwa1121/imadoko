61a8a17c5feced3ae0662d84812a2a2a4de4b9b7
三輪飛翔 <t.miwayagish.jp@PC009A-TMWA.local>
2026-03-14 03:22:23 +0900

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
 export default function Home() {
-  // Mapコンポーネントをブラウザ側でのみ読み込む設定
   const Map = useMemo(
     () =>
       dynamic(() => import("@/app/components/Map"), {
